1432a556a47dd08d135d656314ff89fb81d77fe2
Diomidis Spinellis <dds@aueb.gr>
2023-07-21 09:36:55 +0300

    Relicense under the Apache 2.0 license
    For easy distribution with Open Watcom
    https://github.com/open-watcom/open-watcom-v2/wiki/Relicensing-effort

--- trace.c
+++ trace.c
@@ -1,19 +1,20 @@
 /*
  * trace - trace DOS system calls
  *
- * (C) Copyright 1991-1994 Diomidis Spinellis.  All rights reserved.
+ * (C) Copyright 1991-2023 Diomidis Spinellis
  *
- * $Id: trace.c,v 1.28 1994/10/01 11:32:56 dds Exp $
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Permission to use, copy, and distribute this software and its
- * documentation for any purpose and without fee is hereby granted,
- * provided that the above copyright notice appear in all copies and that
- * both that copyright notice and this permission notice appear in
- * supporting documentation.
- * 
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 #include <stddef.h>
